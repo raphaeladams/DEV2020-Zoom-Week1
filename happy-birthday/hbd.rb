e6264b5bd86d130e1dc7ff42d27ead29c379b624
Raphael Adams <raphael.adams@shopify.com>
2020-10-14 12:22:30 -0400

    Refactored if statements w ternary operator

--- happy-birthday/hbd.rb
+++ happy-birthday/hbd.rb
@@ -5,18 +5,17 @@ print "What is your name? "
 name = gets.chomp
 
 i = 1
+
+
 while i <= 4
 
-  print "Happy birthday "
-
-  if i != 3
-    print "to you"
-    print "." if i == 4
-    print "\n"
-  else
-    print "dear #{ name }\n"
-  end
+  output = "Happy birthday "
+  output += i != 3 ? "to you" : "dear #{ name }"
   
+  print output 
+  print "." if i == 4
+  print "\n"
+
   i += 1
 
 end
