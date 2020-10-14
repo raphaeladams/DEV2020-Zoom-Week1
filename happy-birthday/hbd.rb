5bb04a990df77c9a451b98799c4da4b628a32001
Raphael Adams <raphael.adams@shopify.com>
2020-10-14 10:58:50 -0400

    First commit for Happy Birthday pt 1

---
+++ happy-birthday/hbd.rb
@@ -0,0 +1,10 @@
+# Happy Birthday To You
+# Raphael Adams
+
+print "What is your name? "
+name = gets.chomp
+
+puts "Happy birthday to you"
+puts "Happy birthday to you"
+puts "Happy birthday dear #{ name }"
+puts "Happy birthday to you."
