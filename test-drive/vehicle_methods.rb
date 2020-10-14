91f9d46194de5e47504642966a76db2200464cca
Raphael Adams <raphael.adams@shopify.com>
2020-10-14 14:21:25 -0400

    Added mileage method w return values

--- test-drive/vehicle_methods.rb
+++ test-drive/vehicle_methods.rb
@@ -16,7 +16,20 @@ def use_headlights(brightness = "low-beam")
   puts "Watch out for deer!"
 end
 
+def mileage(miles_driven, gas_used)
+  return 0 if gas_used == 0
+  miles_driven / gas_used
+end
+
 sound_horn
 accelerate
 use_headlights
 use_headlights("high-beam")
+
+trip_mileage  = mileage(400, 12)
+puts "You got #{ trip_mileage } MPG on this trip"
+
+lifetime_mileage = mileage(11432, 366)
+puts "This car averages #{ lifetime_mileage } MPG"
+
+puts mileage(0, 0)
