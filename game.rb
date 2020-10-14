323bc2d26a06e81e99b15072a66da3faad81b993
Raphael Adams <raphael.adams@shopify.com>
2020-10-14 09:21:41 -0400

    Added code to generate/store random number

--- game.rb
+++ game.rb
@@ -7,3 +7,8 @@ print "What's your name? "
 input = gets
 name = input.chomp
 puts "Welcome, #{name}!"
+
+# Store a random number for the player to guess
+puts "I have a random number between 1-100."
+puts "Can you guess it?"
+target = rand(100) + 1
