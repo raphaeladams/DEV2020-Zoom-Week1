baf1ed63cc2951562bea0ba8ee99030f833cefb9
Raphael Adams <raphael.adams@shopify.com>
2020-10-15 15:07:35 -0400

    First commit for RockPaperScissors pt 3

--- rockpaperscissors/rockpaperscissors.rb
+++ rockpaperscissors/rockpaperscissors.rb
@@ -6,31 +6,37 @@ paper = "paper"
 scissors = "scissors"
 
 puts "Rock Paper Scissors"
-print "Player 1 - Enter your selection: "
-player_one_choice = gets.chomp.downcase
 
-print "Player 2 - Enter your selection: "
-player_two_choice = gets.chomp.downcase
+loop do
+    print "Player 1 - Enter your selection: "
+    player_one_choice = gets.chomp.downcase
 
-choices_array = [player_one_choice, player_two_choice]
+    print "Player 2 - Enter your selection: "
+    player_two_choice = gets.chomp.downcase
 
-case
-when (choices_array & [rock, paper, scissors]).any? &&
-    (choices_array.uniq.size <= 1)
-    puts "Tie!"
+    choices_array = [player_one_choice, player_two_choice]
 
-when (choices_array - [rock, paper]).empty?
-    puts "Paper covers rock."
-    puts player_one_choice == paper ? "Player 1 wins." : "Player 2 wins."
-
-when (choices_array - [rock, scissors]).empty?
-    puts "Rock breaks scissors."
-    puts player_one_choice == rock ? "Player 1 wins." : "Player 2 wins."
-
-when (choices_array - [paper, scissors]).empty?
-    puts "Scissors cuts paper."
-    puts player_one_choice == scissors ? "Player 1 wins." : "Player 2 wins."
+    case
+    when (choices_array & [rock, paper, scissors]).any? &&
+        (choices_array.uniq.size <= 1)
+        puts "Tie!"
     
-else
-    puts "No cheaters! Only Rock, Paper or Scissors are allowed."
+    when (choices_array - [rock, paper]).empty?
+        puts "Paper covers rock."
+        puts player_one_choice == paper ? "Player 1 wins." : "Player 2 wins."
+        break
+    
+    when (choices_array - [rock, scissors]).empty?
+        puts "Rock breaks scissors."
+        puts player_one_choice == rock ? "Player 1 wins." : "Player 2 wins."
+        break
+    
+    when (choices_array - [paper, scissors]).empty?
+        puts "Scissors cuts paper."
+        puts player_one_choice == scissors ? "Player 1 wins." : "Player 2 wins."
+        break
+        
+    else
+        puts "No cheaters! Only Rock, Paper or Scissors are allowed."
+    end
 end
