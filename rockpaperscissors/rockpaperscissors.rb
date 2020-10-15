0a0999e9a84a813c59fb451d208dc1d4c8391fc2
Raphael Adams <raphael.adams@shopify.com>
2020-10-15 15:35:51 -0400

    First commit for RockPaperScissors pt 4

--- rockpaperscissors/rockpaperscissors.rb
+++ rockpaperscissors/rockpaperscissors.rb
@@ -4,6 +4,8 @@
 rock = "rock"
 paper = "paper"
 scissors = "scissors"
+player_one_score = 0
+player_two_score = 0
 
 puts "Rock Paper Scissors"
 
@@ -17,26 +19,30 @@ loop do
     choices_array = [player_one_choice, player_two_choice]
 
     case
-    when (choices_array & [rock, paper, scissors]).any? &&
-        (choices_array.uniq.size <= 1)
+    when (choices_array & [rock, paper, scissors]).any? && choices_array.uniq.size <= 1
         puts "Tie!"
     
     when (choices_array - [rock, paper]).empty?
         puts "Paper covers rock."
-        puts player_one_choice == paper ? "Player 1 wins." : "Player 2 wins."
-        break
+        player_one_choice == paper ? player_one_score += 1 : player_two_score += 1
     
     when (choices_array - [rock, scissors]).empty?
         puts "Rock breaks scissors."
-        puts player_one_choice == rock ? "Player 1 wins." : "Player 2 wins."
-        break
-    
+        player_one_choice == rock ? player_one_score += 1 : player_two_score += 1
+
     when (choices_array - [paper, scissors]).empty?
         puts "Scissors cuts paper."
-        puts player_one_choice == scissors ? "Player 1 wins." : "Player 2 wins."
-        break
-        
+        player_one_choice == scissors ? player_one_score += 1 : player_two_score += 1
+
     else
         puts "No cheaters! Only Rock, Paper or Scissors are allowed."
+    end
+
+    if player_one_score >= 2
+        puts "Player 1 wins."
+        break
+    elsif player_two_score >= 2
+        puts "Player 2 wins."
+        break
     end
 end
